ab3619ed6830846b5c55c3037f3b0959820e46c5
Sean Cogan <sean.e.cogan@gmail.com>
2018-07-15 20:44:02 -0400

    Complete Lesson 3 - Animating the Player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    private Animator _anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        var verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (horizontalInput > 0.5 ||
+            horizontalInput < -0.5)
+        {
+            transform.Translate(new Vector3(
+                horizontalInput * MoveSpeed * Time.deltaTime,
+                0));
+        }
+
+        if (verticalInput > 0.5 ||
+            verticalInput < -0.5)
+        {
+            transform.Translate(new Vector3(
+                0,
+                verticalInput * MoveSpeed * Time.deltaTime));
+        }
+
+        _anim.SetFloat("MoveX", horizontalInput);
+        _anim.SetFloat("MoveY", verticalInput);
+    }
+}
