6895d8dfe3badff36279db82a8cc81f27f2a2dc8
Sean Cogan <sean.e.cogan@gmail.com>
2018-07-16 21:02:07 -0400

    Complete Lesson 4 - Blend Tree Animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 
     private Animator _anim;
 
+    private bool _playerIsMoving;
+    private Vector2 _lastMovement;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -17,6 +20,8 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        _playerIsMoving = false;
+
         var horizontalInput = Input.GetAxisRaw("Horizontal");
         var verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -26,6 +31,9 @@ public class PlayerController : MonoBehaviour
             transform.Translate(new Vector3(
                 horizontalInput * MoveSpeed * Time.deltaTime,
                 0));
+
+            _playerIsMoving = true;
+            _lastMovement = new Vector2(horizontalInput, 0);
         }
 
         if (verticalInput > 0.5 ||
@@ -34,9 +42,15 @@ public class PlayerController : MonoBehaviour
             transform.Translate(new Vector3(
                 0,
                 verticalInput * MoveSpeed * Time.deltaTime));
+
+            _playerIsMoving = true;
+            _lastMovement = new Vector2(0, verticalInput);
         }
 
         _anim.SetFloat("MoveX", horizontalInput);
         _anim.SetFloat("MoveY", verticalInput);
+        _anim.SetBool("PlayerIsMoving", _playerIsMoving);
+        _anim.SetFloat("LastMoveX", _lastMovement.x);
+        _anim.SetFloat("LastMoveY", _lastMovement.y);
     }
 }
