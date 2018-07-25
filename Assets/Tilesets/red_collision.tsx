a4da2df3d4db35ec45cc0de3f94fd023b0d53392
Sean Cogan <sean.e.cogan@gmail.com>
2018-07-24 20:40:14 -0400

    Complete Lesson 7 - More Mapping Goodness.

---
+++ Assets/Tilesets/red_collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
+ <image source="../../../../Users/Sean/Desktop/GamesPlusJames - Unity 2D RPG/red-collision-box.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0666667" y="0.0888889" width="15.9333" height="15.8889"/>
+  </objectgroup>
+ </tile>
+</tileset>
