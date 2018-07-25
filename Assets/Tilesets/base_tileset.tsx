8f784ddf74b4c8b38fa4297e7c36cfcc335e63bf
Sean Cogan <sean.e.cogan@gmail.com>
2018-07-24 20:20:24 -0400

    Complete Lesson 6 - Making Tile Maps.

---
+++ Assets/Tilesets/base_tileset.tsx
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="../../../../Users/Sean/Desktop/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0666667" y="0.0222222" width="16.0222" height="16.0222"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="3" x="0.0444444" y="0.0444444" width="15.9111" height="15.9111"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0666667" y="0.0666667" width="15.9778" height="15.9778"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0666667" y="0" width="16.0889" height="16.0889"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0444444" y="0.0222222" width="15.9778" height="15.9778"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0222222" width="15.9778" height="15.9778"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.0222222" width="16.0222" height="16.0222"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0666667" y="0.0444444" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0222222" y="0.0222222" width="15.9333" height="15.9333"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="526">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0222222" y="0.0222222" width="15.9556" height="15.9778"/>
+  </objectgroup>
+ </tile>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0222222" y="0.0444444" width="16" height="15.8667"/>
+  </objectgroup>
+ </tile>
+</tileset>
