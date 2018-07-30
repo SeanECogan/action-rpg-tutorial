c25a48946d2def9c5b5bc82a5e534a0cdbf4fd21
Sean Cogan <sean.e.cogan@gmail.com>
2018-07-30 19:35:07 -0400

    Complete Lesson 8 - Adding Collision Physics and Rigidbody Movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float MoveSpeed;
 
     private Animator _anim;
+    private Rigidbody2D _playerRigidBody;
 
     private bool _playerIsMoving;
     private Vector2 _lastMovement;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
 	void Start ()
     {
         _anim = GetComponent<Animator>();
+        _playerRigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -28,23 +30,35 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput > 0.5 ||
             horizontalInput < -0.5)
         {
-            transform.Translate(new Vector3(
-                horizontalInput * MoveSpeed * Time.deltaTime,
-                0));
+            //transform.Translate(new Vector3(
+            //    horizontalInput * MoveSpeed * Time.deltaTime,
+            //    0));
+
+            _playerRigidBody.velocity = new Vector2(horizontalInput * MoveSpeed, _playerRigidBody.velocity.y);
 
             _playerIsMoving = true;
             _lastMovement = new Vector2(horizontalInput, 0);
+        }
+        else
+        {
+            _playerRigidBody.velocity = new Vector2(0, _playerRigidBody.velocity.y);
         }
 
         if (verticalInput > 0.5 ||
             verticalInput < -0.5)
         {
-            transform.Translate(new Vector3(
-                0,
-                verticalInput * MoveSpeed * Time.deltaTime));
+            //transform.Translate(new Vector3(
+            //    0,
+            //    verticalInput * MoveSpeed * Time.deltaTime));
+
+            _playerRigidBody.velocity = new Vector2(_playerRigidBody.velocity.x, verticalInput * MoveSpeed);
 
             _playerIsMoving = true;
             _lastMovement = new Vector2(0, verticalInput);
+        }
+        else
+        {
+            _playerRigidBody.velocity = new Vector2(_playerRigidBody.velocity.x, 0);
         }
 
         _anim.SetFloat("MoveX", horizontalInput);
